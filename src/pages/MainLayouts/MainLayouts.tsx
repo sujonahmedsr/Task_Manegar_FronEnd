76b99180d6b38ffcb3539e870ec8a491c9217c6c
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-01-18 03:53:21 +0600

    some cng

--- src/pages/MainLayouts/MainLayouts.tsx
+++ src/pages/MainLayouts/MainLayouts.tsx
@@ -5,8 +5,9 @@ const MainLayouts = () => {
     return (
         <div>
             <div className="grid grid-cols-12">
-                <div className="col-span-1  sticky top-0 left-0 h-[100vh] flex flex-col justify-between p-5">
+                <div className="col-span-1  sticky top-0 left-0 h-[100vh] flex flex-col items-center justify-between p-5">
                     <h1>col span 3</h1>
+                    
                     <Button>Log Out</Button>
                 </div>
                 <div className="col-span-8 flex flex-col justify-between ">
