71c718eee0b1d80ce0272b4f0334e8ef26f1e63a
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-01-18 16:43:50 +0600

    chart add using shadcn chart

--- src/pages/MainLayouts/MainLayouts.tsx
+++ src/pages/MainLayouts/MainLayouts.tsx
@@ -4,6 +4,8 @@ import { CgGoogleTasks } from "react-icons/cg";
 import { FaTasks } from "react-icons/fa";
 import { SiGoogletasks } from "react-icons/si";
 import { MdOutlinePending } from "react-icons/md";
+import dummyImg from "../../assets/dummy.png"
+import { Chart } from "../Chart";
 const MainLayouts = () => {
     return (
         <div>
@@ -22,8 +24,50 @@ const MainLayouts = () => {
                 <div className="col-span-8 flex flex-col justify-between">
                     <Outlet />
                 </div>
-                <div className="col-span-3  sticky top-0 left-0 h-[100vh] flex flex-col justify-between p-5">
-                    <h1>col span 3</h1>
+                <div className="col-span-3  sticky top-0 left-0 h-[100vh] flex flex-col p-5 items-center gap-4">
+                    <div>
+                        <Button>Add Task</Button>
+                    </div>
+                    <div className="flex items-center gap-5 px-7 py-5 hover:bg-gray-200 duration-300">
+                        <img className="w-20 h-20 rounded-full" src={dummyImg} alt="dummyImg" />
+                        <div>
+                            <h1 className="text-xl font-semibold">Hello,</h1>
+                            <h1 className="text-2xl font-bold">Sujon Ahmed</h1>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-2 gap-4 w-full">
+                        <div className="font-semibold">
+                            <p className="text-gray-500">Total Tasks:</p>
+                            <p className="pl-4 relative flex gap-2">
+                                <span className="absolute h-[70%] w-[0.2rem] left-1 top-1/2 translate-y-[-50%] bg-purple-500 rounded"></span>
+                                <span className="font-medium text-3xl">10</span>
+                            </p>
+                        </div>
+                        <div className="font-semibold">
+                            <p className="text-gray-500">In Progress:</p>
+                            <p className="pl-4 relative flex gap-2">
+                                <span className="absolute h-[70%] w-[0.2rem] left-1 top-1/2 translate-y-[-50%] bg-purple-500 rounded"></span>
+                                <span className="font-medium text-3xl">5</span>
+                            </p>
+                        </div>
+                        <div className="font-semibold">
+                            <p className="text-gray-500">Open Tasks:</p>
+                            <p className="pl-4 relative flex gap-2">
+                                <span className="absolute h-[70%] w-[0.2rem] left-1 top-1/2 translate-y-[-50%] bg-purple-500 rounded"></span>
+                                <span className="font-medium text-3xl">5</span>
+                            </p>
+                        </div>
+                        <div className="font-semibold">
+                            <p className="text-gray-500">Completed:</p>
+                            <p className="pl-4 relative flex gap-2">
+                                <span className="absolute h-[70%] w-[0.2rem] left-1 top-1/2 translate-y-[-50%] bg-purple-500 rounded"></span>
+                                <span className="font-medium text-3xl">0</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="space-y-3 w-full">
+                        <Chart />
+                    </div>
                 </div>
             </div>
         </div>
